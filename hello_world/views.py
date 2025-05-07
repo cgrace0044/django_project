9327167feb2fd6560f1d9585fcff0209f5d371c9
cgrace0044 <cgrace0044@gmail.com>
2025-05-07 11:49:39 +0200

    update views.py file for error in indentation

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+
     if request.method == "POST":
-       return HttpResponse("You must have POSTed something")
-   else:
-       return HttpResponse(request.method)
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
